4bd47cd2b4e8b26fd84bfebe745b966a09a1c6a8
Akshit Arora <akshit.arora1995@gmail.com>
2016-01-08 18:55:44 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,282 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>CGPCC</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+        
+<meta property="og:url" content="http://cgpcc.thapar.edu/" />
+<meta property="og:title" content="Career Guidance and Personal Counselling Cell" />
+<meta property="og:description" content="
+                                         Welcome to Career Guidance and Personal Counselling Cell, A one of a kind platform that helps you in: Planning Career and Higher Education and Personal counselling. Planning Higher Education and Career: Provide information regarding universities in India and abroad suiting your field and calibre. Help in application process for admission to these universities and institutions. Conducting educational fairs and inviting educational institutions from India and abroad. Psychological counselling: To Improve: Self-concept (Confidence and Esteem) and Interpersonal Relationships and Intrapersonal Relationships. To Deal With: Personal Issues, academic problems, social pressures, addictions, anxiety, insecurity, narcissistic abuse, stress, depression and inferiority complex.
+                                         " />
+<meta property="keywords" content="Career, Guidance, guidance, career, thapar, university, akshit arora, akshit, arora, project, stress, abuse, thapar unviersity, personal, counselling, cell, society, dealing, public, speaking, interpersonal, relationships, loving, art of loving, art of expression, art of communication, SHSS, School, humanities, social, sciences, santha, kumari, hitashi, lomash, simerpreet, shuja, behavior, CGPCC, cgpcc, TU, TU CGPCC, University Guidance, India, abroad, application, internship, MS, Business, intrapersonal, relationship, planning career
+" />
+<meta property="og:type" content="Organization" />
+<meta property="og:image" content="cgpcc12.jpg" />
+        <meta property="og:site_name" content="Career Guidance and Personal Counselling Cell"/>
+<meta name="author" content="Akshit Arora (Core Team Member, CGPCC)" />
+<meta name="apple-mobile-web-app-capable" content="yes" />
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="js/bootstrap.min.js"></script>
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+        <script src="//use.typekit.net/axu4zww.js"></script>
+        <script>try{Typekit.load({ async: true });}catch(e){}</script>
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+        <link rel="shortcut icon" href="images/cgpcc.png" />
+	</head>
+	<body>
+
+		<!-- Header -->
+			<header id="header">
+
+				<!-- Logo -->
+					<h1 id="logo" class="kalam">Career Guidance and Personal Counselling Cell</h1>
+
+				<!-- Nav -->
+					<nav id="nav">
+						<ul>
+							<li><a href="#intro">Intro</a></li>
+							<li><a href="#one">What we do</a></li>
+							<li><a href="#two">Who we are</a></li>
+							<!--<li><a href="#three">Testimonies</a></li>-->
+                            <li><a href="#work">Our work</a></li>
+							<li><a href="#Contact">Contact us</a></li>
+						</ul>
+					</nav>
+
+			</header>
+
+		<!-- Intro -->
+			<section id="intro" class="main style1 dark fullscreen">
+				<div class="content container 70%">
+					<header>
+						<h2>Hey.</h2>
+					</header>
+					<p class="tk-adelle">Welcome to <strong>Career Guidance and Personal Counselling Cell</strong>,<br>A one of a kind platform that helps you in:<br><b>Planning Career and Higher Education</b> and<br><b>Personal counselling.</b>
+</p>
+					<footer>
+						<a href="#one" class="button style2 down">More</a>
+					</footer>
+				</div>
+			</section>
+
+		<!-- One -->
+			<section id="one" class="main style2 right dark fullscreen">
+				<div class="content box style2">
+					<header>
+						<h2>What we do</h2>
+					</header>
+					<p class="tk-adelle">
+                        Planning Higher Education and Career
+                        <ul style="list-style-type:disc">
+                            <li>Provide information regarding universities in India and abroad suiting your field and calibre</li>
+                            <li>Help in application process for admission to these universities and institutions</li>
+                            <li>Conducting educational fairs and inviting educational institutions from India and abroad</li>
+                        </ul>                
+                    </p>
+				</div>
+				<a href="#bla" class="button style2 down anchored">Next</a>
+			</section>
+    <!-- One -->
+			<section id="bla" class="main style2 right dark fullscreen">
+				<div class="content box style2">
+					<header>
+						<h2>What we do</h2>
+					</header>
+					<p class="tk-adelle">
+                        Psychological counselling
+                        <ul style="list-style-type:disc">
+                            <li>To Improve: Self-concept (Confidence and Esteem) and Interpersonal Relationships and Intrapersonal Relationships</li>
+                            <li>To Deal With: Personal Issues, academic problems, social pressures, addictions, anxiety, insecurity, narcissistic abuse, stress, depression and inferiority complex</li>
+                        </ul>                
+                    </p>
+				</div>
+				<a href="#two" class="button style2 down anchored">Next</a>
+			</section>
+
+		<!-- Two -->
+			<section id="two" class="main style2 left dark fullscreen">
+				<div class="content box style2">
+					<header>
+						<h2>Who we are</h2>
+					</header>
+					<p class="tk-adelle">
+                    <h4>Faculty Advisor</h4>
+                    <ul style="list-style-type:disc">
+                        <li>Prof. Santha Kumari (Head, SHSS)</li>
+                        <li>Dr. Maninder Singh (Associate Prof., CSED)</li>
+                        <li>Dr. Manoj Sharma (Associate Prof., SPMS)</li>
+                        <li>Dr. Hitashi Lomash (Asst. Prof., SHSS)</li>
+                        <li>Dr. Simerpreet Ahuja (Asst. Prof., SHSS)</li>
+                        <li>Sharanjeet Kaur (Library)</li>
+                    </ul>
+                    </p>
+				</div>
+                <a href="#blatwo" class="button style2 down anchored">Next</a>
+            </section>
+<!-- BlaTwo -->
+			<section id="blatwo" class="main style2 left dark fullscreen">
+				<div class="content box style2">
+					<header>
+						<h2>Who we are</h2>
+					</header>
+					<p class="tk-adelle">
+                    <h4>Core Team</h4>
+                    <ul style="list-style-type:disc">
+                        <li>Akash Aggarwal (B.E. ECE, Batch 2017)</li>
+                        <li>Aman Preet (M.A. Psychology, Batch 2017)</li>
+                        <li>Aastha Agarwal (B.E. ECE, Batch 2017)</li>
+                        <li>Abhinav Garg (B.E. ECE, Batch 2017)</li>
+                        <li>Akshit Arora (B.E. COE, Batch 2017)</li>
+                    </ul>
+                    </p>
+				</div>
+                <a href="#work" class="button style2 down anchored">Next</a>
+            </section>
+
+		<!-- Work -->
+			<section id="work" class="main style3 primary">
+				<div class="content container">
+					<header>
+						<h2>Our work</h2>
+						<p>And now, please have a look at what we have been doing and what we plan to do in future.</p>
+					</header>
+
+					<!-- Lightbox Gallery  -->
+                    <p>PAST EVENTS</p>
+						<div class="container 75% gallery">
+							<div class="row 0% images">
+								<div class="6u 12u(mobile)"><a href="images/fulls/01a.jpg" class="image fit from-left"><img src="images/thumbs/01a.jpg" title="Art of Loving" alt="" /></a></div>
+								<div class="6u 12u(mobile)"><a href="images/fulls/02a.jpg" class="image fit from-right"><img src="images/thumbs/02a.jpg" title="Art of Loving Details" alt="" /></a></div>
+							</div>
+							<div class="row 0% images">
+								<div class="6u 12u(mobile)"><a href="images/fulls/03a.jpg" class="image fit from-left"><img src="images/thumbs/03a.jpg" title="Orientation Program" alt="" /></a></div>
+								<div class="6u 12u(mobile)"><a href="images/fulls/04a.jpg" class="image fit from-right"><img src="images/thumbs/04a.jpg" title="Orientation Program Details" alt="" /></a></div>
+							</div>
+							<div class="row 0% images">
+								<div class="6u 12u(mobile)"><a href="images/fulls/05a.jpg" class="image fit from-left"><img src="images/thumbs/05a.jpg" title="Summer Training Information Seminar" alt="" /></a></div>
+                                <div class="6u 12u(mobile)"><a href="images/fulls/06a.jpg" class="image fit from-right"><img src="images/thumbs/06a.jpg" title="Seminar Details" alt="" /></a></div>
+						</div>
+                    <!-- Lightbox Gallery  -->
+                    <br>
+                    <p>UPCOMING EVENTS</p>
+						<div class="container 75% gallery">
+                            <p>THIS MONTH</p>
+                            <ul>
+                                <li>9 SEPT  <b>ART OF COMMUNICATION</b></li>
+                                <li>29 SEPT <b>ART OF EXPRESSION</b></li></ul>
+                            <p>UPCOMING</p>
+                            <ul>
+                                <li>OCT 2015 <b>CAREER DECISIONS AND FUTURE PLANS (3rd Year only)</b></li>
+                                <li>OCT 2015 <b>SATURNALIA EVENT</b></li>
+                                <li>FEB 2016 <b>SYMPOSIUM (Details will be uploaded soon)</b></li>
+                            </ul>
+							</div>
+						</div>
+
+				</div>
+			</section>
+
+		<!-- Contact us -->
+			<section id="Contact" class="main style3 secondary">
+				<div class="content container">
+					<header>
+						<h2>Say Hello.</h2>
+						<p>You get into contact with any of our following members.</p>
+					</header>
+                    <div class="row box container 90%">
+                        <div class="col-md-4">
+                            <blockquote>
+                                Prof. Santha Kumari<br>
+                                santha@thapar.edu<br>
+                                Head,  SHSS<br>
+                                Room No 122<br>
+                                G Block<br>
+                            </blockquote>
+                        </div>
+                        <div class="col-md-4">
+                            <blockquote>
+                                Dr. Hitashi Lomash<br>
+                                hitashi@thapar.edu<br>
+                                Asst. Prof., SHSS<br>
+                                Room no 129<br>
+                                G Block<br>
+                            </blockquote>
+                        </div>
+                        <div class="col-md-4">
+                            <blockquote>
+                                Dr. Simerpreet Ahuja<br>
+                                sahuja@thapar.edu<br>
+                                Asst. Prof. SHSS<br>
+                                Room no 129<br>
+                                G Block<br>
+                            </blockquote>
+                        </div>
+                    </div>
+                    <div class="row box container 90%">
+                        <div class="col-md-4">
+                            <blockquote>
+                                Akash Aggarwal<br>
+                                ECE Undergraduate<br>
+                                +91-819 802 9325<br>
+                                agarwal.akash1995@gmail.com<br>
+                            </blockquote>
+                        </div>
+                        <div class="col-md-4">
+                            <blockquote>
+                                Abhinav Garg<br>
+                                ECE Undergraduate<br>
+                                +91-965 495 5623<br>
+                                abhinavgarg95@gmail.com<br>
+                            </blockquote>
+                        </div>
+                        <div class="col-md-4">
+                            <blockquote>
+                                Akshit Arora<br>
+                                COE Undergraduate<br>
+                                +91-769 606 1995<br>
+                                akshit.arora1995@gmail.com<br>
+                            </blockquote>
+                        </div>
+
+                    </div>
+				</div>
+			</section>
+
+		<!-- Footer -->
+			<footer id="footer">
+
+				<!-- Icons -->
+					<ul class="actions">
+                        <li><a href="https://www.facebook.com/pages/CGPC-cellTU/1502591923360482?fref=ts" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+						
+					</ul>
+
+				<!-- Menu -->
+					<ul class="menu">
+						<li>&copy; CGPCC</li><li>Design Credits: <a href="http://html5up.net">HTML5 UP</a> | <a href="https://www.facebook.com/AkshitArora1995">Akshit Arora</a></li>
+					</ul>
+
+			</footer>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.poptrox.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
